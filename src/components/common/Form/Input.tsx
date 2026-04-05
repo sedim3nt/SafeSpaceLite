81d7b66f77182d8cbdf21b2d6daad724770fa395
sedim3nt <terraloam-eye@gmail.com>
2026-04-05 00:32:54 -0600

    fix: center date input text

--- src/components/common/Form/Input.tsx
+++ src/components/common/Form/Input.tsx
@@ -8,6 +8,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export function Input({ label, error, helperText, className = '', id, ...props }: InputProps) {
   const inputId = id || `input-${Math.random().toString(36).substr(2, 9)}`;
+  const isDateInput = props.type === 'date';
 
   return (
     <div className="space-y-1.5">
@@ -22,6 +23,10 @@ export function Input({ label, error, helperText, className = '', id, ...props }
           error
             ? 'border-red-300 focus:border-red-500 focus:ring-red-500/20'
             : 'border-border focus:border-sage-400 focus:ring-sage-400/20'
+        } ${
+          isDateInput
+            ? 'py-0 leading-none [&::-webkit-date-and-time-value]:min-h-[48px] [&::-webkit-date-and-time-value]:flex [&::-webkit-date-and-time-value]:items-center [&::-webkit-date-and-time-value]:text-left [&::-webkit-datetime-edit]:flex [&::-webkit-datetime-edit]:h-full [&::-webkit-datetime-edit]:items-center'
+            : ''
         } ${className}`}
         {...props}
       />
